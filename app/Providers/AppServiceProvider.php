6693dbdf0fd83a77e9a79ba0d73c95119de0075f
hamza <echchablihamza1@gmail.com>
2025-03-05 14:08:23 +0000

    commit changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,20 +3,30 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\MessageService;
+use App\Services\PostService;
+use App\Services\LmkopService;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        // Liaison du service dans le container
+        app()->singleton('message', function () {
+            return new MessageService();
+        });
+        app()->singleton('post', function () {
+            return new PostService();
+        });
+        app()->singleton('lmkop', function () {
+            return new LmkopService();
+        });
+        
     }
-
-    /**
-     * Bootstrap any application services.
-     */
+    
     public function boot(): void
     {
         //
